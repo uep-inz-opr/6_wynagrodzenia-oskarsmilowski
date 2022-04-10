2e87596565b65cee446a85caa85471cfad2965aa
Oskar <69815@student.ue.poznan.pl>
2022-04-10 16:31:18 +0200

    do sprawdzenia

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
 
-@dataclass(frozen=True, order=True)
+@dataclass(frozen=True)
 class Pracownik:
     imie: str
     wynagrodzenie_brutto: int
 
     def wynagrodzenie_netto(self) -> float:
-        poz_c = self.wynagrodzenie_brutto * 0.1371
+        poz_c = round(self.wynagrodzenie_brutto * 0.0976,2) + round(self.wynagrodzenie_brutto * 0.015,2) + round(self.wynagrodzenie_brutto * 0.0245,2)
         poz_c = round(poz_c,2)
 
-        poz_d = self.wynagrodzenie_brutto - poz_c
+        poz_d = self.wynagrodzenie_brutto - round(poz_c,2)
+        poz_d = round(poz_d,2)
 
         poz_e = poz_d * 0.09
         poz_e = round(poz_e,2)
@@ -19,30 +20,40 @@ class Pracownik:
 
         poz_g = 111.25
 
-        poz_h = self.wynagrodzenie_brutto - poz_g - poz_c
+        poz_h = self.wynagrodzenie_brutto - poz_g
+        poz_h = round(poz_h, 2)
+        poz_h -= round(poz_c,2)
         poz_h = round(poz_h, 2)
         
         poz_h_18 = poz_h * 0.18
         poz_h_18 = round(poz_h_18, 2)
         poz_i = poz_h_18 - 46.33
+        poz_i = round(poz_i,2)
 
-        poz_j = poz_i - poz_f
+        poz_j = round(poz_i,2) - round(poz_f,2)
         poz_j = round(poz_j,2)
         
-
-        netto = self.wynagrodzenie_brutto - poz_c - poz_e - poz_j
-        netto = round(netto, 2)
+        netto = self.wynagrodzenie_brutto
+        netto -= round(poz_c,2)
+        netto = round(netto,2)
+        netto -= round(poz_e,2)
+        netto = round(netto,2)
+        netto -= round(poz_j,2)
+        netto = round(netto,2)
+        
         return netto
     
     def skladki_pracodawcy(self) -> float:
-        poz_i = self.wynagrodzenie_brutto * 0.2074
+        poz_i = round(self.wynagrodzenie_brutto * 0.0976,2) + round(self.wynagrodzenie_brutto * 0.065,2) + round(self.wynagrodzenie_brutto * 0.0193,2) + round(self.wynagrodzenie_brutto * 0.0245,2) + round(self.wynagrodzenie_brutto * 0.001,2)
         poz_i = round(poz_i,2)
         return poz_i
     
     def koszt_pracodawcy(self) -> float:
-        poz_i = self.wynagrodzenie_brutto * 0.2074
+        poz_i = round(self.wynagrodzenie_brutto * 0.0976,2) + round(self.wynagrodzenie_brutto * 0.065,2) + round(self.wynagrodzenie_brutto * 0.0193,2) + round(self.wynagrodzenie_brutto * 0.0245,2) + round(self.wynagrodzenie_brutto * 0.001,2)
         poz_i = round(poz_i,2)
-        return self.wynagrodzenie_brutto + poz_i
+        koszt = self.wynagrodzenie_brutto + poz_i
+        koszt = round(koszt,2)
+        return koszt
 
 
 
@@ -63,7 +74,7 @@ def main():
         skladki = pracownik.skladki_pracodawcy()
         koszt = pracownik.koszt_pracodawcy()
         laczny_koszt += koszt
-        print(pracownik.imie, netto,skladki,koszt)
+        print(pracownik.imie,netto,skladki,koszt)
     
     print(laczny_koszt)
 
